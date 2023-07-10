7d7c9e6d6eeb30fd3279fdb9de34c9b81fdd7d55
杜宇 <dygood@outlook.com>
2023-07-10 11:22:39 +0800

    feat: 添加GridFS使用例子.

---
+++ example/MongoGridFS.Example/ServiceModules/MongoFSModule.cs
@@ -0,0 +1,36 @@
+﻿using EasilyNET.AutoDependencyInjection.Contexts;
+using EasilyNET.AutoDependencyInjection.Extensions;
+using EasilyNET.AutoDependencyInjection.Modules;
+using EasilyNET.MongoGridFS.AspNetCore;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using MongoDB.Driver;
+
+namespace MongoGridFS.Example;
+
+/// <summary>
+/// GridFS
+/// </summary>
+public class MongoFSModule : AppModule
+{
+    /// <inheritdoc />
+    public MongoFSModule()
+    {
+        Enable = false;
+    }
+
+    /// <inheritdoc />
+    public override void ConfigureServices(ConfigureServicesContext context)
+    {
+        var db = context.Services.GetService<IMongoDatabase>() ?? throw new("请先注册IMongoDatabase服务");
+        context.Services.Configure<FormOptions>(c =>
+               {
+                   c.MultipartHeadersLengthLimit = int.MaxValue;
+                   c.MultipartBodyLengthLimit = long.MaxValue;
+                   c.ValueLengthLimit = int.MaxValue;
+               })
+               .Configure<KestrelServerOptions>(c => c.Limits.MaxRequestBodySize = null)
+               .Configure<IISServerOptions>(c => c.MaxRequestBodySize = null);
+        context.Services.AddMongoGridFS(db);
+    }
+}
