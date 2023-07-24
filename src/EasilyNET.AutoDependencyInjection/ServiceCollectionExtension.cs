a932d4cdfdb7f2f73fe759460ac31a4ec91b691b
杜宇 <dygood@outlook.com>
2023-07-24 17:06:39 +0800

    feat: 回退代码到1.6.7

--- src/EasilyNET.AutoDependencyInjection/ServiceCollectionExtension.cs
+++ src/EasilyNET.AutoDependencyInjection/ServiceCollectionExtension.cs
@@ -5,6 +5,7 @@ using EasilyNET.AutoDependencyInjection.Modules;
 using EasilyNET.AutoDependencyInjection.PropertyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
 
 // ReSharper disable UnusedMethodReturnValue.Global
@@ -57,16 +58,21 @@ public static partial class ServiceCollectionExtension
     }
 
     /// <summary>
+    /// 添加属性注入服务
+    /// </summary>
+    /// <param name="mvcBuilder">mvc构建器</param>
+    /// <returns></returns>
+    public static IMvcBuilder AddPropertyInjectionAsServices(this IMvcBuilder mvcBuilder)
+    {
+        mvcBuilder.AddControllersAsServices();
+        mvcBuilder.Services.Replace(ServiceDescriptor.Transient<IControllerActivator, PropertyInjectionControllerActivator>());
+        return mvcBuilder;
+    }
+
+    /// <summary>
     /// 使用属性注入
     /// </summary>
     /// <param name="hostBuilder">host构建器</param>
     /// <returns></returns>
-    public static void UsePropertyInjection(this IHostBuilder hostBuilder)
-    {
-        hostBuilder.UseServiceProviderFactory(new PropertyInjectionServiceProviderFactory()).ConfigureServices(ConfigureServices);
-    }
-
-    private static void ConfigureServices(IServiceCollection services) =>
-        services.AddSingleton<IPropertyInjector, PropertyInjector>()
-                .AddSingleton<IControllerFactory, PropertyInjectionControllerFactory>();
+    public static void UsePropertyInjection(this IHostBuilder hostBuilder) => hostBuilder.UseServiceProviderFactory(new PropertyInjectionServiceProviderFactory());
 }
