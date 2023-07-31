9d17e72caa19e722dff1b2636f26447633d7e597
杜宇 <dygood@outlook.com>
2023-07-31 15:00:24 +0800

    :recycle: 将字符串的MD5提升到Core项目,扩大适用范围.

--- src/EasilyNET.Core/Misc/StringExtension.cs
+++ src/EasilyNET.Core/Misc/StringExtension.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Specialized;
 using System.Globalization;
+using System.Security.Cryptography;
 using System.Text;
 using System.Text.Json;
 using System.Text.RegularExpressions;
@@ -700,5 +701,29 @@ public static class StringExtension
             c[j++] = (b & 0x0f).ToHexChar();
         }
         return new(c);
+    }
+
+    /// <summary>
+    /// 获取16位长度的MD5大写字符串
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    public static string To16MD5(this string value) => value.To32MD5().Substring(8, 16);
+
+    /// <summary>
+    /// 获取32位长度的MD5大写字符串
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    public static string To32MD5(this string value)
+    {
+        var data = MD5.HashData(Encoding.UTF8.GetBytes(value));
+        var builder = new StringBuilder();
+        // 循环遍历哈希数据的每一个字节并格式化为十六进制字符串 
+        foreach (var t in data)
+        {
+            _ = builder.Append(t.ToString("X2"));
+        }
+        return builder.ToString();
     }
 }
--- src/EasilyNET.Security/README.md
+++ src/EasilyNET.Security/README.md
@@ -2,7 +2,7 @@
 
 一个.Net 中常用的加密算法的封装.降低加密解密的使用复杂度.
 
-- 目前有的算法:AES,DES,SHA,RC4,MD5,TripleDES,RSA,SM2
+- 目前有的算法:AES,DES,SHA,RC4,TripleDES,RSA,SM2,SM3
 - 支持 RSA XML 结构的 SecurityKey 和 Base64 格式的互转.
 
 - 本库不是去实现加密算法,而是基于.Net 提供的接口封装,为了方便使用
