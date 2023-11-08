8db3505b2c8b9885cfae8264394ad09d768ea82d
Joes <dygood@outlook.com>
2023-11-08 18:16:22 +0800

    更新

--- sample/EntityFrameworkCore.Test.Unit/Controllers/WeatherForecastController.cs
+++ sample/EntityFrameworkCore.Test.Unit/Controllers/WeatherForecastController.cs
@@ -4,33 +4,26 @@ namespace EntityFrameworkCore.Test.Unit.Controllers;
 
 /// <summary>
 /// </summary>
+/// <remarks>
+/// </remarks>
+/// <param name="logger"></param>
 [ApiController, Route("[controller]")]
-public class WeatherForecastController : ControllerBase
+public class WeatherForecastController(ILogger<WeatherForecastController> logger) : ControllerBase
 {
     private static readonly string[] Summaries =
     {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-    private readonly ILogger<WeatherForecastController> _logger;
-
-    /// <summary>
-    /// </summary>
-    /// <param name="logger"></param>
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
-    {
-        _logger = logger;
-    }
-
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
+                         {
+                             Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                             TemperatureC = Random.Shared.Next(-20, 55),
+                             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                         })
                          .ToArray();
     }
 }
