6d5915d691052dcf30e8b351fe8c57d5eed0fee7
dong <44517714+lurudong@users.noreply.github.com>
2023-11-10 20:29:05 +0800

    添加 EF CORE迁移接口

---
+++ src/EasilyNET.EntityFrameworkCore/Migrations/IMigrationService.cs
@@ -0,0 +1,12 @@
+﻿namespace EasilyNET.EntityFrameworkCore.Migrations;
+
+/// <summary>
+/// 迁移
+/// </summary>
+public interface IMigrationService
+{
+    /// <summary>
+    /// 安装dotnet-ef
+    /// </summary>
+    public void InstallEfTool();
+}
---
+++ src/EasilyNET.EntityFrameworkCore/Migrations/MigrationService.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace EasilyNET.EntityFrameworkCore.Migrations;
+
+/// <summary>
+/// 自动迁移
+/// </summary>
+public class MigrationService : IMigrationService
+{
+    /// <summary>
+    /// 安装dotnet-ef工具
+    /// </summary>
+    public void InstallEfTool()
+    {
+        var startInfo = new ProcessStartInfo("dotnet tool", "install --global dotnet-ef")
+        {
+            UseShellExecute = false, // 使用操作系统的外壳来启动
+            RedirectStandardInput = true,
+            RedirectStandardOutput = true,
+            CreateNoWindow = true,
+            StandardOutputEncoding = Encoding.UTF8,
+            StandardErrorEncoding = Encoding.UTF8
+        };
+        var process = new Process();
+        process.StartInfo = startInfo;
+        process.Start();
+    }
+}
