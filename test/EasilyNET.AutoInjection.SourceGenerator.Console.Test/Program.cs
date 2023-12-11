29b0d6b221f21d828a7f0dd66f8341b39a859f49
dong <44517714+lurudong@users.noreply.github.com>
2023-12-12 04:17:17 +0800

    添加自动注入代码生成器

---
+++ test/EasilyNET.AutoInjection.SourceGenerator.Console.Test/Program.cs
@@ -0,0 +1,85 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using EasilyNET.AutoDependencyInjection.Core.Abstractions;
+using EasilyNET.AutoDependencyInjection.Core.Attributes;
+using EasilyNET.AutoInjection.SourceGenerator.Console.Test;
+using Microsoft.Extensions.DependencyInjection;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        IServiceCollection serviceCollection = new ServiceCollection();
+        var service = serviceCollection.AddAutoInjection();
+        var provider = service.BuildServiceProvider();
+        var test1 = provider.GetService<ITest<User>>();
+        Console.WriteLine(test1!.GetTest());
+        var test2 = provider.GetService<ITest1<User, User>>();
+        Console.WriteLine(test2!.GetTest());
+        Console.ReadKey();
+    }
+}
+
+//public static class _Dependency
+//{
+//    public static IServiceCollection AddAutoInjection1(this IServiceCollection services)
+//    {
+//        services.Add(new(typeof(ITestTransient), typeof(TestTransient), ServiceLifetime.Singleton));
+//        return services;
+//    }
+//}
+
+public interface ITestTransient : IAA1p, ITransientDependency;
+
+public interface ITestScoped : IScopedDependency;
+
+public interface ITestSingleton : ISingletonDependency;
+
+public class TestTransient : ITestTransient { }
+
+public class TestTransient1 : ITestTransient { }
+
+public class TestScoped : ITestScoped { }
+
+public class TestScoped1 : ITestScoped { }
+
+public class TestSingleton : ITestSingleton { }
+
+public interface IAA;
+
+public class AA : IAA { }
+
+public interface IAA1p { }
+
+public class User { }
+
+public interface ITest<T> : ITransientDependency
+{
+    string GetTest();
+}
+
+public class Test<User> : ITest<User>
+{
+    public string GetTest() => "Test";
+}
+
+public interface ITest1<T, T1> : ITransientDependency
+{
+    string GetTest();
+}
+
+public class Test1<T, T1> : ITest1<T, T1>
+{
+    public string GetTest() => "Test1";
+}
+
+[DependencyInjection(ServiceLifetime.Scoped)]
+public class Test3 { }
+
+[DependencyInjection(ServiceLifetime.Scoped, AddSelf = true)]
+public class Test4 { }
+
+[DependencyInjection(ServiceLifetime.Scoped)]
+public class Test5 : ITest5 { }
+
+public interface ITest5 { }
