141c0d715144237433d85691dd7947c5e54aff43
dong <44517714+lurudong@users.noreply.github.com>
2023-12-18 19:32:27 +0800

    添加单元测试

---
+++ test/EasilyNET.AutoInjection.SourceGenerator.MsTest.Tests/AutoInjectionTest.cs
@@ -0,0 +1,110 @@
+using EasilyNET.AutoDependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EasilyNET.AutoInjection.SourceGenerator.MsTest.Tests;
+
+/// <summary>
+/// µ¥Ôª²âÊÔ
+/// </summary>
+[TestClass]
+public class AutoInjectionTest
+{
+    /// <summary>
+    /// </summary>
+    [TestMethod]
+    public void TestTransient()
+    {
+        using var application = ApplicationFactory.Create<TestAppModule>();
+        var test = application.ServiceProvider!.GetService<ITestTransient>();
+        var re = test?.GetTest("´ó»Æ¹Ï");
+        // Assert
+        Assert.IsTrue(re == $"{nameof(ITestTransient)}_´ó»Æ¹Ï");
+    }
+
+    /// <summary>
+    /// </summary>
+    [TestMethod]
+    public void TestScoped()
+    {
+        using var application = ApplicationFactory.Create<TestAppModule>();
+        var test = application.ServiceProvider!.GetService<ITestScoped>();
+        var re = test?.GetTest("´ó»Æ¹Ï");
+        // Assert
+        Assert.IsTrue(re == $"{nameof(ITestScoped)}_´ó»Æ¹Ï");
+    }
+
+    /// <summary>
+    /// </summary>
+    [TestMethod]
+    public void TestSingleton()
+    {
+        using var application = ApplicationFactory.Create<TestAppModule>();
+        var test = application.ServiceProvider!.GetService<ITestSingleton>();
+        var re = test?.GetTest("´ó»Æ¹Ï");
+        // Assert
+        Assert.IsTrue(re == $"{nameof(ITestSingleton)}_´ó»Æ¹Ï");
+    }
+}
+
+/// <summary>
+/// </summary>
+public interface ITestTransient : ITransientDependency
+{
+    /// <summary>
+    /// </summary>
+    /// <param name="name"></param>
+    string GetTest(string name);
+}
+
+/// <summary>
+/// </summary>
+public class TestTransient : ITestTransient
+{
+    /// <summary>
+    /// </summary>
+    /// <param name="name"></param>
+    /// <returns></returns>
+    public string GetTest(string name) => $"{nameof(ITestTransient)}_{name}";
+}
+
+/// <summary>
+/// </summary>
+public interface ITestScoped : IScopedDependency
+{
+    /// <summary>
+    /// </summary>
+    /// <param name="name"></param>
+    string GetTest(string name);
+}
+
+/// <summary>
+/// </summary>
+public class TestScoped : ITestScoped
+{
+    /// <summary>
+    /// </summary>
+    /// <param name="name"></param>
+    /// <returns></returns>
+    public string GetTest(string name) => $"{nameof(ITestScoped)}_{name}";
+}
+
+/// <summary>
+/// </summary>
+public interface ITestSingleton : ISingletonDependency
+{
+    /// <summary>
+    /// </summary>
+    /// <param name="name"></param>
+    string GetTest(string name);
+}
+
+/// <summary>
+/// </summary>
+public class TestSingleton : ITestSingleton
+{
+    /// <summary>
+    /// </summary>
+    /// <param name="name"></param>
+    /// <returns></returns>
+    public string GetTest(string name) => $"{nameof(ITestSingleton)}_{name}";
+}
---
+++ test/EasilyNET.AutoInjection.SourceGenerator.MsTest.Tests/GlobalUsings.cs
@@ -0,0 +1,4 @@
+global using EasilyNET.AutoDependencyInjection.Contexts;
+global using EasilyNET.AutoDependencyInjection.Core.Abstractions;
+global using EasilyNET.AutoDependencyInjection.Modules;
+global using Microsoft.VisualStudio.TestTools.UnitTesting;
