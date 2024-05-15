a34a1dde2eaaee5e0d8affd985a6468967a292f7
Joes <dygood@outlook.com>
2024-05-15 18:17:20 +0800

    style: format code

--- sample/WebApi.Test.Unit/Script.js
+++ sample/WebApi.Test.Unit/Script.js
@@ -1,15 +1,15 @@
-﻿import http from 'k6/http';
-import {check} from 'k6';
+﻿import { check } from "k6";
+import http from "k6/http";
 
 export const options = {
-    stages: [
-        {duration: '30s', target: 20},
-        {duration: '1m30s', target: 10},
-        {duration: '20s', target: 0},
-    ],
+  stages: [
+    { duration: "30s", target: 20 },
+    { duration: "1m30s", target: 10 },
+    { duration: "20s", target: 0 },
+  ],
 };
 
 export default function () {
-    const res = http.post('http://localhost:5046/api/MongoTest/MongoPost');
-    check(res, {'status was 200': (r) => r.status == 200});
+  const res = http.post("http://localhost:5046/api/MongoTest/MongoPost");
+  check(res, { "status was 200": (r) => r.status == 200 });
 }
