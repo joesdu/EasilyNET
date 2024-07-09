955e14abe5568e78472a24a89300702fc3f1641f
Joes <dygood@outlook.com>
2024-07-09 10:54:35 +0800

    feat: 添加平台标识,为不同平台代码可做预编译指令.

---
+++ sample/WebApi.Test.Unit/Common/SysHelper.cs
@@ -0,0 +1,19 @@
+﻿#if Windows
+using System.Runtime.Versioning;
+using System.Security.Principal;
+#endif
+
+namespace WebApi.Test.Unit.Common;
+
+internal static class SysHelper
+{
+#if Windows
+    [SupportedOSPlatform("windows")]
+    internal static bool IsCurrentUserAdmin()
+    {
+        using var identity = WindowsIdentity.GetCurrent();
+        var principal = new WindowsPrincipal(identity);
+        return principal.IsInRole(WindowsBuiltInRole.Administrator);
+    }
+#endif
+}
