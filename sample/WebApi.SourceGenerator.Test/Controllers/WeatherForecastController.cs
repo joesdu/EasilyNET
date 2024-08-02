01d508d589d68a032e49bc99293732f3b6605aa8
Joes <dygood@outlook.com>
2024-08-02 10:02:15 +0800

    feat: 更新依赖,移除SG的实现,项目没有移除,后期再继续学习

--- sample/WebApi.SourceGenerator.Test/Controllers/WeatherForecastController.cs
+++ sample/WebApi.SourceGenerator.Test/Controllers/WeatherForecastController.cs
@@ -1,4 +1,3 @@
-using System.ComponentModel;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebApi.SourceGenerator.Test.Controllers;
@@ -14,34 +13,4 @@ public class WeatherForecastController(WeatherForecastService wfs) : ControllerB
     /// <returns></returns>
     [HttpGet("GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get() => wfs.Get();
-
-    /// <summary>
-    /// </summary>
-    /// <returns></returns>
-    [HttpGet("GetEnumDescription")]
-    public string GetEnumDescription() => ETestEnumDescription.A.ToDescription();
-}
-
-/// <summary>
-/// 测试枚举源码生成器
-/// </summary>
-public enum ETestEnumDescription
-{
-    /// <summary>
-    /// A
-    /// </summary>
-    [Description("ADES")]
-    A,
-
-    /// <summary>
-    /// B
-    /// </summary>
-    [Description("BDES")]
-    B,
-
-    /// <summary>
-    /// C
-    /// </summary>
-    [Description("CDES")]
-    C
 }
---
+++ src/EasilyNET.Core/Misc/EnumExtensions.cs
@@ -0,0 +1,23 @@
+﻿// ReSharper disable UnusedMember.Global
+// ReSharper disable UnusedType.Global
+
+using System.ComponentModel;
+
+namespace EasilyNET.Core.Misc;
+
+/// <summary>
+/// 扩展枚举
+/// </summary>
+public static class EnumExtensions
+{
+    /// <summary>
+    /// 转成显示名字
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    public static string ToDescription(this Enum value)
+    {
+        var fieldInfo = value.GetType().GetField(value.ToString())?.GetCustomAttributes(typeof(DescriptionAttribute), false).FirstOrDefault();
+        return fieldInfo is DescriptionAttribute descriptionAttribute ? descriptionAttribute.Description : value.ToString();
+    }
+}
