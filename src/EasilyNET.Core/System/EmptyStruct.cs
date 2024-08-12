d10dd24229dc8e0c3e4ba76f98bf85891e8b1e28
Joes <dygood@outlook.com>
2024-08-12 11:49:13 +0800

    feat: 新增AsyncBarrier异步屏障,它会阻塞一组任务,直到所有任务都到达,通常用于需要协调多个异步任务的场景,确保所有任务在某个同步点之前都已完成特定的工作,然后再继续执行

---
+++ src/EasilyNET.Core/System/EmptyStruct.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+// ReSharper disable UnusedMember.Global
+
+namespace EasilyNET.Core.System;
+
+/// <summary>
+/// 一个空结构体
+/// </summary>
+/// <remarks>
+/// 当泛型类型需要一个类型参数但完全不使用时,这可以节省 4 个字节,相对于 System.Object
+/// </remarks>
+internal readonly struct EmptyStruct
+{
+    /// <summary>
+    /// 获取空结构体的一个实例
+    /// </summary>
+    internal static EmptyStruct Instance => default;
+}
