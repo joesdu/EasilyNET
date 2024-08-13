d923c6e5112ce7bd8dbbd2a5dc43345483b38593
Joes <dygood@outlook.com>
2024-08-13 15:08:34 +0800

    feat: 新增枚举扩展函数,获取所有枚举值列表,并支持排除部分项.

--- src/EasilyNET.Core/Misc/EnumExtensions.cs
+++ src/EasilyNET.Core/Misc/EnumExtensions.cs
@@ -20,4 +20,16 @@ public static class EnumExtensions
         var fieldInfo = value.GetType().GetField(value.ToString())?.GetCustomAttributes(typeof(DescriptionAttribute), false).FirstOrDefault();
         return fieldInfo is DescriptionAttribute descriptionAttribute ? descriptionAttribute.Description : value.ToString();
     }
+
+    /// <summary>
+    /// 获取枚举所有值，排除指定的值
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="exclude">要排除的枚举值</param>
+    /// <returns>排除指定值后的枚举值集合</returns>
+    public static IEnumerable<T> GetValues<T>(params T[] exclude) where T : Enum
+    {
+        var allValues = Enum.GetValues(typeof(T)).Cast<T>();
+        return allValues.Except(exclude);
+    }
 }
