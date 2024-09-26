8576dd591c2c258152c9f1586c0abf24e406ad82
Psyche <lbhdr@outlook.com>
2024-09-26 18:07:24 +0800

    fix: attribute params type no support

--- src/EasilyNET.Mongo.Core/TimeSeriesCollectionAttribute.cs
+++ src/EasilyNET.Mongo.Core/TimeSeriesCollectionAttribute.cs
@@ -16,7 +16,7 @@ namespace EasilyNET.Mongo.Core
         /// <summary>
         /// 可选。通过指定文档过期后的秒数，启用自动删除时间序列集合中文档的功能。MongoDB 自动删除过期文档。请参阅设置自动删除时间序列集合 (TTL)，获取更多信息。
         /// </summary>
-        public TimeSpan? ExpireAfter {  get; set; }
+        public TimeSpan? ExpireAfter { get; set; }
 
         /// <summary>
         /// 标记为时序集合
@@ -26,7 +26,7 @@ namespace EasilyNET.Mongo.Core
         /// <param name="granularity">表示MongoDB.Driver.TimeSeriesGranularity时间序列的粒度。如果使用bucketMaxSpanSeconds，则不设置</param>
         /// <param name="bucketMaxSpanSeconds">同一存储桶中时间戳之间的最大时间间隔。</param>
         /// <param name="bucketRoundingSeconds">打开新存储桶时用于四舍五入第一个时间戳的间隔。</param>
-        public TimeSeriesCollectionAttribute(string timeField, Optional<string> metaField = default, Optional<TimeSeriesGranularity?> granularity = default, Optional<int?> bucketMaxSpanSeconds = default, Optional<int?> bucketRoundingSeconds = default)
+        public TimeSeriesCollectionAttribute(string timeField, string? metaField = default, TimeSeriesGranularity? granularity = default, int? bucketMaxSpanSeconds = default, int? bucketRoundingSeconds = default)
         {
             TimeSeriesOptions = new TimeSeriesOptions(timeField, metaField, granularity, bucketMaxSpanSeconds, bucketRoundingSeconds);
         }
