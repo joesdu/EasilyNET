ade28e6c1fc76189553b9f5e505f99e9560104f5
Joes <dygood@outlook.com>
2024-10-15 11:13:37 +0800

    feat: 添加Garnet的简单用法.

---
+++ sample/WebApi.Test.Unit/ServiceModules/GarnetModule.cs
@@ -0,0 +1,17 @@
+﻿using EasilyNET.AutoDependencyInjection.Contexts;
+using EasilyNET.AutoDependencyInjection.Modules;
+using StackExchange.Redis;
+
+namespace WebApi.Test.Unit.ServiceModules;
+
+internal sealed class GarnetModule : AppModule
+{
+    public override void ConfigureServices(ConfigureServicesContext context)
+    {
+        var garnet = context.Services.GetConfiguration().GetConnectionString("Garnet") ?? throw new("无法读取到连接字符串");
+        var configurationOptions = ConfigurationOptions.Parse(garnet);
+        configurationOptions.ClientName = "Garnet";
+        var client = ConnectionMultiplexer.ConnectAsync(configurationOptions);
+        context.Services.AddSingleton<IConnectionMultiplexer>(client.Result);
+    }
+}
