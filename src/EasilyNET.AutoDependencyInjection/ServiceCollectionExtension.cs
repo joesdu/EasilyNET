443aba28b8e26b92d2b5929ca96f8f82a1db7ade
Joes <dygood@outlook.com>
2024-12-27 13:13:24 +0800

    feat: 屏蔽掉部分命名空间与文件夹结构不匹配的类,这些类为注册服务使用,统一到微软的DependencyInjection名称空间.

--- src/EasilyNET.AutoDependencyInjection/ServiceCollectionExtension.cs
+++ src/EasilyNET.AutoDependencyInjection/ServiceCollectionExtension.cs
@@ -8,6 +8,8 @@ using Microsoft.Extensions.Hosting;
 
 // ReSharper disable UnusedMember.Global
 
+#pragma warning disable IDE0130 // 命名空间与文件夹结构不匹配
+
 namespace Microsoft.Extensions.DependencyInjection;
 
 /// <summary>
--- src/EasilyNET.Mongo.AspNetCore/ServiceCollectionExtensions.cs
+++ src/EasilyNET.Mongo.AspNetCore/ServiceCollectionExtensions.cs
@@ -13,6 +13,8 @@ using MongoDB.Driver;
 // ReSharper disable UnusedMember.Global
 // ReSharper disable MemberCanBePrivate.Global
 
+#pragma warning disable IDE0130 // 命名空间与文件夹结构不匹配
+
 namespace Microsoft.Extensions.DependencyInjection;
 
 /// <summary>
